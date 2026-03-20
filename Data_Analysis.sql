f5cfd65fb2c112b5bc1cf314267621e1c72aa948
AnthonyRefugio <refugioanthony356@gmail.com>
2026-03-20 16:36:28 -0400

    Update Data_Analysis.sql

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -1,10 +1,32 @@
--- Data Analysis Company layoffs
+/* Layoff Trends Analysis
+- Which industries were hit the hardest
+- Which countries had the most layoffs
+- What years/months saw the most layoffs
+- Are layoffs increasing or decreasing over time 
+*/
 
-
-SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+SELECT *
 FROM layoffs_staging2;
 
 
-SELECT *
+
+-- 1. What industries were hit the hardest 
+SELECT SUM(total_laid_off), SUM(percentage_laid_off)
+FROM layoffs_staging2;
+
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+
+SELECT 
+    industry,
+    SUM(total_laid_off) AS total_layoffs,
+    ROUND(SUM(total_laid_off) * 100 / (SELECT SUM(total_laid_off) FROM layoffs_staging2), 2) AS percentage_of_total
 FROM layoffs_staging2
-WHERE company = 'Block';
+WHERE industry IS NOT NULL
+GROUP BY industry
+ORDER BY total_layoffs DESC;
+
+
+
