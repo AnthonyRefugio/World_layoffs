1bea07509d6b1c7e9a4bbf35fcdd377bb66f9f2c
AnthonyRefugio <refugioanthony356@gmail.com>
2026-03-20 18:15:58 -0400

    Finished are layoffs increasing or decreasing over time

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -78,3 +78,31 @@ SELECT
     SUM(total_layoffs) OVER (ORDER BY month ASC) AS rolling_total
 FROM rolling_total
 ORDER BY month ASC;
+
+
+
+-- 4. Are layoffs increasing or decreasing over time 
+
+SELECT
+    YEAR(`date`) AS year,
+    SUM(total_laid_off) AS total_layoffs,
+    LAG(SUM(total_laid_off)) OVER (ORDER BY YEAR(`date`)) AS previous_year,
+    SUM(total_laid_off) - LAG(SUM(total_laid_off)) OVER (ORDER BY YEAR(`date`)) AS difference,
+    ROUND((SUM(total_laid_off) - LAG(SUM(total_laid_off)) OVER (ORDER BY YEAR(`date`))) * 100 / 
+    LAG(SUM(total_laid_off)) OVER (ORDER BY YEAR(`date`)), 2) AS percentage_change 
+FROM layoffs_staging2
+WHERE `date` IS NOT NULL
+GROUP BY YEAR(`date`)
+ORDER BY year ASC;
+
+
+SELECT
+    YEAR(`date`) AS year,
+    COUNT(DISTINCT company) AS companies_affected,
+    SUM(total_laid_off) AS total_layoffs,
+    ROUND(AVG(total_laid_off), 0) AS avg_layoffs_per_compnay,
+    ROUND(AVG(percentage_laid_off)* 100, 0) AS avg_percentage_laid_off
+FROM layoffs_staging2
+WHERE `date` IS NOT NULL
+GROUP BY YEAR(`date`) 
+ORDER BY year ASC;
